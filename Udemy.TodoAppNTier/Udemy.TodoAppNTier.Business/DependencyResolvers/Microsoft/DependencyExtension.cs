0c0df22cfc4d6c59bd472de0fead8bd987402b8e
BBerkinT <topaloglubatinberkin@gmail.com>
2024-11-14 23:24:46 +0300

    NLayered
    Entities-Dependencies-Repositories-UnitOfWork-GetAll

---
+++ Udemy.TodoAppNTier/Udemy.TodoAppNTier.Business/DependencyResolvers/Microsoft/DependencyExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Udemy.TodoAppNTier.DataAccess.Contexts;
+using Udemy.TodoAppNTier.DataAccess.UnitofWork;
+using Udemy.ToDoAppNTier.Business.Interfaces;
+using Udemy.ToDoAppNTier.Business.Services;
+
+namespace Udemy.ToDoAppNTier.Business.DependencyResolvers.Microsoft
+{
+    public static class DependencyExtension
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt =>
+            {
+                opt.UseSqlServer("server=(localdb)\\mssqllocaldb; database=TodoDb;integrated security=true;");
+            });
+            services.AddScoped<IUow, Uow>();
+            services.AddScoped<IWorkService, WorkService>();
+
+
+        }
+    }
+}
