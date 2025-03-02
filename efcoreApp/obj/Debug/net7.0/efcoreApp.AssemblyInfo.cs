21e2bac40899fb20d05fdf7dc84ca9dc34c8f2fe
BBerkinT <topaloglubatinberkin@gmail.com>
2025-03-02 18:38:06 +0300

    Kayıt Ekleme

--- efcoreApp/obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
+++ efcoreApp/obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8b62e56c1f5053440d31c876bea7ce68b0cf2aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6646245e4cee6d8aa610f105e152eb10c8471c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
