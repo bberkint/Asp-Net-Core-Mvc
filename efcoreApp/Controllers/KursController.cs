f2897369d1b0a1b3e93cc52cf85970219f4abb92
BBerkinT <topaloglubatinberkin@gmail.com>
2025-03-03 15:08:44 +0300

    Kurs Kayıt Sayfası (Create)

---
+++ efcoreApp/Controllers/KursController.cs
@@ -0,0 +1,33 @@
+﻿using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace efcoreApp.Controllers
+{
+    public class KursController : Controller
+    {
+        private readonly DataContext _context;
+        public KursController(DataContext context)
+        {
+            _context = context; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Kurs model) { 
+            
+            _context.Kurslar.Add(model);
+            await _context.SaveChangesAsync();  
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
