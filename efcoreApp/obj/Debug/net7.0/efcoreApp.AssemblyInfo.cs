39f6ad698b9c8a9829be42186a918bc9cefaf99f
BBerkinT <topaloglubatinberkin@gmail.com>
2025-03-04 02:58:31 +0300

    Kurs Ogrencilerinin Listelenmesi

--- efcoreApp/obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
+++ efcoreApp/obj/Debug/net7.0/efcoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1cc2124db0279ea070b6fd632ce46cf3380a849")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3acfa896bef84c67487d260ea2060f13ac84efdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("efcoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
