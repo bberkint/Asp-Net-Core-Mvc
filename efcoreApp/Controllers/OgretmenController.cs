700a1e3b7a38b02f551e5d94f255cf185af1f97d
BBerkinT <topaloglubatinberkin@gmail.com>
2025-03-05 00:46:36 +0300

    Ogretmen Ekleme listeleme Ve Güncelleme

---
+++ efcoreApp/Controllers/OgretmenController.cs
@@ -0,0 +1,93 @@
+﻿using efcoreApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcoreApp.Controllers
+{
+    public class OgretmenController : Controller
+    {
+
+        private readonly DataContext _context;
+        public OgretmenController(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ogretmenler = await _context.Ogretmenler.ToListAsync();
+
+            return View(ogretmenler);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Ogretmen model)
+        {
+            _context.Ogretmenler.Add(model);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var entity = await _context.Ogretmenler.FirstOrDefaultAsync(o => o.OgretmenId == id);
+            //var ogr = await _context.Ogrenciler.FirstOrDefaultAsync(ogrenci => ogrenci.OgrenciId == id);
+
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            return View(entity);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Ogretmen model)
+        {
+
+            if (id != model.OgretmenId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(model);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Ogretmenler.Any(o => o.OgretmenId == model.OgretmenId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+
+    }
+}
--- efcoreApp/Data/Ogretmen.cs
+++ efcoreApp/Data/Ogretmen.cs
@@ -7,9 +7,12 @@ namespace efcoreApp.Data
         [Key]
         public int OgretmenId { get; set; }
         public string? Ad { get; set; }
-        public string?  Soyad { get; set; }
+        public string? Soyad { get; set; }
         public string? Eposta { get; set; }
         public string? Telefon { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy.MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime BaslamaTarihi { get; set; }
         public ICollection<Kurs> Kurslar { get; set; } = new List<Kurs>();
     }
