96a429f7f6fe8c448e66a15f3f7282db16960c7d
BBerkinT <topaloglubatinberkin@gmail.com>
2025-03-14 21:11:09 +0300

    Tags (VC)

--- BlogApp/obj/Debug/net7.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net7.0/BlogApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c2c2080ad4c3e1cab98ace70866338ec7e35e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f6031ad77163325626ae6a9a59f29529bf00fd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
