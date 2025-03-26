97d65acb19e54da2ecfd82bfbb0976c90d09e730
BBerkinT <topaloglubatinberkin@gmail.com>
2025-03-26 19:18:36 +0300

    SeedData Update

--- BlogApp/obj/Debug/net7.0/BlogApp.AssemblyInfo.cs
+++ BlogApp/obj/Debug/net7.0/BlogApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602427d0113cccecdb0bc2925b005ce06800ce12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492f37eafdee4df4f68e14b5de07343715bd5e41")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
